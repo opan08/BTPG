85d1e0d00a33ff7a8667883270450c80e538a8a9
jiangzeyu <jiangzeyu@gziis.org>
2024-10-11 15:10:58 +0800

    add commits

--- src/BTPG.cpp
+++ src/BTPG.cpp
@@ -125,7 +125,7 @@ BTPG::BTPG(std::string fileName, int mode, int timeInterval)
                 int biPairNum = getNumBiPairs();//记录当前bidirectional pair的数量
                 if (group->type2Edges[0]->isBidirectional)
                     continue;
-                CheckSingleton(group->type2Edges[0]);//检测单向type2边是否可以形成bidirectional pair
+                CheckSingleton(group->type2Edges[0]);//检测单向type2边是否可以形成bidirectional pair，可以形成则添加双向对
                 // 如果增加了新的bidirectional pair，则addMorePairs计数加1
                 if (biPairNum != getNumBiPairs())
                 {
@@ -228,7 +228,7 @@ void BTPG::CheckSingleton(type2Edge *candidateEdge)
 }
 
 /**
- * @brief 这个才是BTPG的核心，论文中的算法1，
+ * @brief 这个才是BTPG的核心，论文中的算法1，这里只是将新的方向边添加到type2的集合，并且设置成对应的边是bidirectional.
  * 
  * @param candidateEdge 为原来的type2边(（(v^m_i+1, v^(n)_(j))）)
  * @return true 可以形成bidirectional pair
@@ -249,13 +249,13 @@ bool BTPG::CheckSingletonValidity(type2Edge *candidateEdge)
     }
 
     // Temporarily set the edge to be bidirectional
-    candidateEdge->isBidirectional = true;
+    candidateEdge->isBidirectional = true;//设置为bidirectional
     // Temporarily add a new edge to type-2 edge 算法1第4行，新建一条反向的type2边（(v^n_j+1, v^m_i)）
     type2Edge *newType2Edge = new type2Edge();
     newType2Edge->nodeFrom = endNode;
     newType2Edge->nodeTo = startNode;
     newType2Edge->edgeId = getNumTypeTwoEdges();
-    newType2Edge->isBidirectional = true;
+    newType2Edge->isBidirectional = true;//设置为bidirectional
     addTypeTwoEdge(newType2Edge);//算法1第5行，添加到type2边的集合中
 
     // 1b. Initialize the visit stacks for nodes 记录所有已经访问过的节点
@@ -269,7 +269,7 @@ bool BTPG::CheckSingletonValidity(type2Edge *candidateEdge)
         setVisitedNode = setVisitedNode->Type1Prev;
     }
 
-    // 1c. Initialize the stacks for revisit nodes
+    // 1c. Initialize the stacks for revisit nodes 需要重新访问的节点
     std::vector<std::set<Node *>> revisitedNodes;
     for (int i = 0; i < getNumTypeTwoEdges(); i++)
     {
@@ -278,8 +278,8 @@ bool BTPG::CheckSingletonValidity(type2Edge *candidateEdge)
     }
 
     // 1d. stack for current recursion path
-    std::vector<Node *> recursionPath;//记录已经访问过的节点
-    std::set<Node *> recursionPathSet;
+    std::vector<Node *> recursionPath;//记录当前迭代的路径
+    std::set<Node *> recursionPathSet;//
 
     // 1e. stack for edges in the current recursion path
     std::set<int> recursionEdgePath;
@@ -330,14 +330,14 @@ bool BTPG::CheckSingletonValidity(type2Edge *candidateEdge)
 /**
  * @brief 深度搜索DFS, 论文中的算法2 hasCycle函数，判断是否存在NRNS cycle
  * 
- * @param currNode_ 当前节点
- * @param endNode_ 目标节点
- * @param VisitedStack_ 
- * @param RevisitedNodes_ 
- * @param RecursionPath_  记录已经访问过的边，对应论文中的E_vis
- * @param RecursionPathSet_ 记录了遍历的所有节点
- * @param AgentEdgeMap_ 记录从那条边进入到这个agent的<robotId, edgeId>
- * @param AgentEdgeMapLeave_ 记录从哪条边离开这个agent的<robotId, edgeId>
+ * @param currNode_ 当前节点v^m_i
+ * @param endNode_ 目标节点v^n_j+1
+ * @param VisitedStack_ 记录已经访问的节点，对应论文中的E_vis
+ * @param RevisitedNodes_ 记录需要重新访问的节点，对应[edgeId][node]
+ * @param RecursionPath_  记录当前递归的路径
+ * @param RecursionPathSet_ 记录当前递归的路径，是RecursionPath_的set类型，用于更快第查重
+ * @param AgentEdgeMap_ 记录从哪条type2边进入到这个agent的<robotId, edgeId>
+ * @param AgentEdgeMapLeave_ 记录从哪条type2边离开这个agent的<robotId, edgeId>
  * @param hasTYpe1Edge_ 上边遍历的边是否含有type1边
  * @return true 包含NRNS cycle
  * @return false 不包含NRNS cycle
@@ -350,22 +350,35 @@ bool BTPG::BidirectionalDFS(Node *currNode_, Node *endNode_, std::set<Node *> &V
     // 1. check if reach the end node
     if (currNode_ == endNode_)
     {
-        // 如果设置了hasTYpe1Edge_，表示这个cycle中带有type1边，则不是rotation cycle,属于non-rotation cycle，则返回true
+        // 如果设置了hasTYpe1Edge_，表示这个cycle中带有type1边，则不是rotation cycle,属于non-rotation cycle，
+        // 由于同一个bidirectional pair的另一条边在下面已经判断，所以也不可能是self-cycle，属于non-self cycle，综上，这里则返回true
         // rotaion cycle定义是只含有type2边，而且边的数目大于2。
-        if (hasTYpe1Edge_ || RecursionPath_.size() > 2)
+        if (hasTYpe1Edge_ || RecursionPath_.size() > 2) //TODO: 确认以下这个>2的判断条件是否正确，应该是<3才对
         {
             // print out the recursion path
+            // std::cout << "hasTYpe1Edge_:" << hasTYpe1Edge_ << std::endl;
             // std::cout << "Recursion Path: ";
             // for (auto &node : RecursionPath_)
             // {
-            //     std::cout << node->robotId << " " << node->timeStep << " -> ";
+            //     std::cout << node->robotId << " " << node->timeStep << "(" << node->coord.x << "," << node->coord.y << ")" <<" -> ";
             // }
             // std::cout << std::endl;
-            // std::cout << "endNode: " << endNode_->robotId << " " << endNode_->timeStep << std::endl;
-            return true;//TODO:论文中，算法2第3行，这里应该返回false才对，但是这里返回了true，为什么相反了？？
+            // std::cout << "endNode: " << endNode_->robotId << " " << endNode_->timeStep << "(" << endNode_->coord.x << "," << endNode_->coord.y << ")" << std::endl;
+            return true;
         }
         else
         {
+            // if(RecursionPath_.size() <= 2)
+            // {
+            //     std::cout << "Recursion Path: ";
+            //     for (auto &node : RecursionPath_)
+            //     {
+            //         std::cout << node->robotId << " " << node->timeStep << "(" << node->coord.x << "," << node->coord.y << ")" <<" -> ";
+            //     }
+            //     std::cout << std::endl;
+            //     std::cout << "endNode: " << endNode_->robotId << " " << endNode_->timeStep << "(" << endNode_->coord.x << "," << endNode_->coord.y << ")" << std::endl;
+            // }
+            // 如果不包含type1的边，且长度小于等于2，则表明形成了一个rotation cycle，所以返回false。参考论文的图2
             // revisit all the nodes in the recursion path
             for (auto node = RecursionPath_.rbegin(); node != RecursionPath_.rend() - 1; node++)
             {
@@ -376,39 +389,49 @@ bool BTPG::BidirectionalDFS(Node *currNode_, Node *endNode_, std::set<Node *> &V
         }
     }
 
-    // Update
+    // Update 将当前节点加入已经访问的节点集合中，已经加入当前迭代的路径中
     VisitedStack_.insert(currNode_);
     RecursionPath_.push_back(currNode_);//记录当前迭代的路径
     RecursionPathSet_.insert(currNode_);
 
-    // !: Traverse Type-2 Edge 遍历从currNode_出发的所有type2边
+    // !: 步骤一，遍历type2的边，Traverse Type-2 Edge 遍历从currNode_出发的所有type2边。这个可以对着论文中图5进行理解。
     for (auto &edge : currNode_->Type2Next)
     {
         // 2. Checking if need to keep going on
-        // 判断edge->nodeTo节点是否已经访问过（判断edge->nodeTo是否在VisitedStack_和RecursionPathSet_中）算法2第8行
+        // 判断type2边的终点节点edge->nodeTo是否已经访问过（判断edge->nodeTo是否在VisitedStack_和RecursionPathSet_中）算法2第8行
         if (VisitedStack_.find(edge->nodeTo) == VisitedStack_.end() && RecursionPathSet_.find(edge->nodeTo) == RecursionPathSet_.end())
         {
+            // 如果没有被访问过，则继续迭代
             // Need keep visiting next node
-            // 2a. if so, checking if the edge is bidirectional 如果没有访问过，则判断是否是bidirectional pair
+            // 2a. if so, checking if the edge is bidirectional 如果没有访问过，则判断是否是bidirectional pair。下面的条件是为了过滤self-cycle
+            // 这里只需要判断是否是bidirectional pair中的边就行，因为TPG原来是没有cycle的，增加了反向边之后，形成了cycle，就判断这个cycle是否是NRNS cycle就可以
+            // 从上面C到下面B的边是bidirectional pair的，所以会进入下面
             if (edge->isBidirectional)//论文中算法2第7行，如果这条边已经属于bidirectional pair
             {
-                if (this->mode == 0 && AgentEdgeMap_[currNode_->robotId] != -1)
+                // 如果这条边属于bidirectional pair，则判断之前是否记录了哪个type2的边进入这个agent
+                // AgentEdgeMap_ 记录从哪条type2边进入到这个agent的<robotId, edgeId>
+                if (this->mode == 0 && AgentEdgeMap_[currNode_->robotId] != -1) //如果使用BTPG-n，而且有记录进入这个agent的边
                 {
+                    // 如果记录了哪个type2边进入这个agent，则获得这个边的终点节点
                     // Only check if the previous edge is the same bidirectional edge
-                    Node *prevNode = getTypeTwoEdge(AgentEdgeMap_[currNode_->robotId])->nodeTo;
+                    Node *prevNode = getTypeTwoEdge(AgentEdgeMap_[currNode_->robotId])->nodeTo;//获取到达这个agent的type2边的终点节点
                     if (edge->biPairId == getTypeTwoEdge(AgentEdgeMap_[currNode_->robotId])->biPairId)
                     {
-                        // 如果这条边跟之前的边属于同一个bidirectional pair
+                        // 如果这条边跟之前的边属于同一个bidirectional pair，则表明形成了一个self cycle，不需要继续迭代这条type2边
                         // !: if reach current Node by a type-1 edge, then we should only revisit current node
                         // 如果通过type1边到达当前节点
+                        // TODO:确认以下判断条件是否正确，上面就将currNode_加入到RecursionPath_中，这个条件一直成立才对。如果按照注释，这个应该是back的前一个才对
                         if (RecursionPath_.back()->robotId == currNode_->robotId)
                         {
+                            // 将当前节点加入到RevisitedNodes_[进入到当前agent的type2边]中
                             RevisitedNodes_[AgentEdgeMap_[currNode_->robotId]].insert(currNode_);
                         }
                         else
                         {
                             // !: if reach current Node by a type-2 edge,
                             // !then we should revisit all the nodes in the recursion path back to the prevNode
+                            // 如果通过type2边到达当前节点，则需要回溯当前路径，将当前路径的所有节点都加入到需要重新访问的节点集合RevisitedNodes_中
+                            std::cout << "reach current Node by a type-2 edge" << std::endl;
                             for (auto renode = RecursionPath_.rbegin(); renode != RecursionPath_.rend(); renode++)
                             {
                                 RevisitedNodes_[AgentEdgeMap_[currNode_->robotId]].insert(*renode);
@@ -418,7 +441,7 @@ bool BTPG::BidirectionalDFS(Node *currNode_, Node *endNode_, std::set<Node *> &V
                                 }
                             }
                         }
-                        continue;
+                        continue;//如果是双向对，证明找到self cycle，则直接进入下一个边，不再迭代
                     }
                 }
                 // 2a.1: Check if we should visit the edge (BTPG-o)
@@ -454,6 +477,7 @@ bool BTPG::BidirectionalDFS(Node *currNode_, Node *endNode_, std::set<Node *> &V
                     }
                 }
             }
+            //如果不是双向，则会直接进入这里，继续进行递归下一个节点
 
             // 2a.2: Check if we should visit the edge (BTPG-o)
             if (this->mode == 1 && AgentEdgeMapLeave_[edge->nodeTo->robotId] != -1)
@@ -478,13 +502,13 @@ bool BTPG::BidirectionalDFS(Node *currNode_, Node *endNode_, std::set<Node *> &V
 
             if (edge->nodeTo->robotId != -1)
             {
-                // 2b.1: update agentEdgeMap (enter) 记录从edge->edgeId进入到edge->nodeTo的robotId
+                // 2b.1: update agentEdgeMap (enter) 记录从edge->edgeId进入到agent
                 AgentEdgeMap_[edge->nodeTo->robotId] = edge->edgeId;
             }
-            // 2b.2: update agentEdgeMap (leave) 记录从edge->edgeId离开currNode_的robotId
+            // 2b.2: update agentEdgeMap (leave) 记录从edge->edgeId离开了agent
             AgentEdgeMapLeave_[currNode_->robotId] = edge->edgeId;
 
-            // !: c.Recursion
+            // !: c.Recursion 继续迭代，将边的终点作为新的起点
             if (BidirectionalDFS(edge->nodeTo, endNode_, VisitedStack_, RevisitedNodes_, RecursionPath_, RecursionPathSet_, AgentEdgeMap_, AgentEdgeMapLeave_, hasTYpe1Edge_))
             {
                 // 如果能找到NRNS cycle，则返回true
@@ -499,7 +523,7 @@ bool BTPG::BidirectionalDFS(Node *currNode_, Node *endNode_, std::set<Node *> &V
             // 2b.4: update agentEdgeMap (leave), without the edge in the recursion path
             AgentEdgeMapLeave_[currNode_->robotId] = -1;
 
-            // revisit relevant nodes
+            // revisit relevant nodes 从VisitedStack_中删除需要重新访问的节点
             for (auto &node : RevisitedNodes_[edge->edgeId])
             {
                 VisitedStack_.erase(node);
@@ -508,19 +532,20 @@ bool BTPG::BidirectionalDFS(Node *currNode_, Node *endNode_, std::set<Node *> &V
         }
         else
         {
-            // 如果这个节点已经访问过，则继续下一个边
+            // 如果这个节点已经访问过，则继续下一个边，避免无限迭代
             continue;
         }
     }
 
-    // !: Traverse Type-1 Edge 遍历从currNode_出发的所有type1边
+    // !: 步骤2，遍历type1的边，Traverse Type-1 Edge 遍历从currNode_出发的所有type1边，由于type1的边只有1条，所以不需要使用for循环
     if (currNode_->Type1Next != NULL)
     {
-        // 1. check if need to keep going on
+        // 1. check if need to keep going on 判断这条边的终点是否已经遍历了
         if (VisitedStack_.find(currNode_->Type1Next) == VisitedStack_.end() && RecursionPathSet_.find(currNode_->Type1Next) == RecursionPathSet_.end())
         {
-            // 如果currNode_->Type1Next不存在于VisitedStack_和RecursionPathSet_中，则继续迭代
+            // 如果这条边的终点currNode_->Type1Next节点不存在于VisitedStack_和RecursionPathSet_中，则继续迭代
             // !: c.Recursion
+            // 最后一个参数设置为true，表示这条边是type1边
             if (BidirectionalDFS(currNode_->Type1Next, endNode_, VisitedStack_, RevisitedNodes_, RecursionPath_, RecursionPathSet_, AgentEdgeMap_, AgentEdgeMapLeave_, true))
             {
                 // 如果在迭代中找到NRNS cycle，则返回true
@@ -529,6 +554,7 @@ bool BTPG::BidirectionalDFS(Node *currNode_, Node *endNode_, std::set<Node *> &V
         }
     }
 
+    // 如果迭代type1跟type2的边都没有找到NRNS cycle，则从路径中删除当前节点，并返回false
     // Update
     RecursionPath_.pop_back();
     RecursionPathSet_.erase(currNode_);
